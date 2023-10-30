c3413b8b7d195aea8bb2fabe3917e44351860917
Martina Tavernelli <martinabelentavernelli@gmail.com>
2023-10-30 13:00:24 +0100

    fix: fixing img src

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,5 @@
 const Loading = () => {
-  return <img src="dist/loading.svg" alt="Loading..." height="100px" />;
+  return <img src="loading.svg" alt="Loading..." height="100px" />;
 };
 
 export default Loading;
